06a2e409508a2165cb46a67e5a022d0e184e65a3
mgs28-mh <gsaputra880@gmail.com>
2025-09-01 21:02:58 +0700

    blog: update metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,12 @@
 import AboutSection from "@/components/about";
 import Hero from "@/components/about/hero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "https://archipelago.web.id/about",
+  },
+};
 
 export default function About() {
     return (
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,12 @@
 import BlogPage from "@/components/blog-page";
 import Hero from "@/components/blog/hero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "https://archipelago.web.id/blog",
+  },
+};
 
 export default function Blog() {
     return (
