5a3b01ddfd8e5ba4a0992bce4eb6f0f0b7b7bfa2
mgs28-mh <gsaputra880@gmail.com>
2025-09-05 15:48:33 +0700

    blog: update footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -55,7 +55,7 @@ export default function Footer() {
   };
 
   return (
-    <footer className="bg-teal-950 text-white">
+    <footer className="bg-slate-800/50 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* ====== TOP SECTION ====== */}
         <div className="py-16">
@@ -84,7 +84,7 @@ export default function Footer() {
                   <Link
                     key={item.href}
                     href={item.href}
-                    className={`text-gray-300 hover:text-white transition-colors duration-200 text-base font-medium ${active ? "text-white" : ""
+                    className={`text-gray-300 hover:text-white transition-colors duration-200 text-md uppercase font-medium ${active ? "text-white" : ""
                       }`}
                   >
                     {item.label}
@@ -101,7 +101,7 @@ export default function Footer() {
                   href={social.href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="p-2 text-gray-400 hover:text-white transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-slate-900 rounded-lg"
+                  className="p-2 text-white/90 hover:text-white transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-slate-900 rounded-lg"
                   aria-label={social.label}
                 >
                   {social.icon}
@@ -114,7 +114,7 @@ export default function Footer() {
           <div className="border-t border-slate-700 mb-8" />
 
           {/* ====== BOTTOM SECTION ====== */}
-          <div className="flex items-center justify-center text-gray-400 text-sm">
+          <div className="flex items-center justify-center text-gray-400 text-md">
             {/* Copyright */}
             <p>© {currentYear} Kata Komunikasi.</p>
           </div>
