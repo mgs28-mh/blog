1011a85c4a36974891c7faed5018a221bcc182f6
mgs28-mh <gsaputra880@gmail.com>
2025-09-06 21:45:39 +0700

    blog: update hero

--- components/blog/hero.tsx
+++ components/blog/hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative h-[60vh] md:h-[70vh] py-12 flex items-center justify-center overflow-hidde bg-lime-100">
+    <section className="relative h-[50vh] md:h-[70vh] py-12 flex items-center justify-center overflow-hidde bg-lime-100">
       {/* Content Container */}
       <div className="relative z-20 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Main Heading */}
@@ -15,7 +15,7 @@ export default function Hero() {
           transition={{ duration: 0.8, delay: 0.2 }}
           className="text-center max-w-5xl mx-auto"
         >
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-slate-950 leading-tight">
+          <h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-slate-950 leading-tight">
             Blog & Artikel{" "}
             <span className="text-green-600">Terbaru</span>
           </h1>
